9afd435a5046c4788ccc97047125a6b73b3a09cb
rpeterson87 <oshorhino5053@gmail.com>
2022-09-16 19:05:09 -0400

    routes complete and tested

--- controllers/photography-controller.js
+++ controllers/photography-controller.js
@@ -2,26 +2,52 @@ const express = require("express")
 const router = express.Router()
 const Photography = require('../models/Photography')
 
-
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
-// Index Route
+// Photography Index Route
 router.get('/', async (req, res) => {
     try {
-        res.status(200).json(await Photography.find({message: "This is a test"}))
+        res.status(200).json(await Photography.find({}))
     }catch(error){
-        console.log(error)
+        res.status(200).json(error)
+    }
+})
+
+// Photography Post Route
+router.post('/', async (req, res) => {
+    try {
+        res.status(201).json(await Photography.create(req.body))
+    }catch(error){
+        res.status(201).json(error)
     }
 })
 
 // Photography Show Route
-router.post('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        res.status(200).json( await Photography.create(req.body))
+        res.json(await Photography.findById(req.params.id))
     }catch(error){
-        console.log(error)
+        res.status(400).json(error)
+    }
+})
+
+// Photography Update Route
+router.put('/:id', async (req, res) => {
+    try {
+        res.json(await Photography.findByIdAndUpdate(req.params,id, req.body, {new:true}))
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+// Photography Delete Route
+router.delete('/:id', async (req, res) => {
+    try {
+        res.json(await Photography.findByIdAndRemove(req.params.id))
+    }catch(error){
+        res.status(400).json(error)
     }
 })
 
